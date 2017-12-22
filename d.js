2f5f0f1eb1f861a0693115fcc4b8c31c56b082a6
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-22 20:56:04 +0700

    abcd

--- d.js
+++ d.js
@@ -1,3 +1,9 @@
+/*
+    class Line
+    2 thuoc pointA, pointB
+    getLength -> return do dai doan thang
+*/
+
 class Point {
     constructor(x, y) {
         this.x = x;
@@ -12,7 +18,44 @@ class Point {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 }
+// class Triangle pointA, pointB, pointC -> getPerimeter
+class Line {
+    constructor(pointA, pointB) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
 
-const pointA = new Point(1, 1);
-console.log(pointA.getDistance());
-console.log(pointA.distance);
+    get length() {
+        const dx = this.pointA.x - this.pointB.x;
+        const dy = this.pointA.y - this.pointB.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+class Triangle {
+    constructor(pointA, pointB, pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
+    }
+
+    get perimeter() {
+        const lAB = new Line(this.pointA, this.pointB);
+        const lAC = new Line(this.pointA, this.pointC);
+        const lBC = new Line(this.pointB, this.pointC);
+        return lAB.length + lAC.length + lBC.length;
+    }
+}
+const pA = new Point(0, 0);
+const pB = new Point(1, 0);
+const pC = new Point(0, 1);
+const tABC = new Triangle(pA, pB, pC);
+console.log(tABC.perimeter);
+
+
+// const pA = new Point(0, 2);
+// const pB = new Point(0, 1);
+// const lAB = new Line(pA, pB);
+// console.log(lAB.length);
+// console.log(pointA.getDistance());
+// console.log(pointA.distance);
