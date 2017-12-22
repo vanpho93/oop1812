c9c6ad9cd55759249de96305fa335d98aa2e1b74
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-22 19:50:05 +0700

    backstick

--- a.js
+++ a.js
@@ -1,8 +1,16 @@
-const teo = { name: 'Teo Nguyen', age: 10 };
-
+const teo = {
+    name: 'Teo Nguyen',
+    age: 10,
+    sayHello: function () {
+        // console.log('Xin chao. toi la ' + this.name + ', toi ' + this.age + ' tuoi');
+        console.log(`Xin chao toi la ${this.name}, toi ${this.age} tuoi`);
+    }
+};
+teo.sayHello();
 console.log(teo);
 // teo = {};
 teo.age = 11;
 
 console.log(teo.name);
 console.log(teo.age);
+teo.sayHello();
